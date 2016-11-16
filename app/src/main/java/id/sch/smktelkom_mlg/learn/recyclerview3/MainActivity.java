fbcb322836c1ac3a02f9197044d57f2abfdd220f
RinkoYukina <yukakanama@gmail.com>
2016-11-17 03:32:27 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -0,0 +1,35 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
+import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
+
+public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
+
+
+    int itemPos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(this, mList);
+        recyclerView.setAdapter(mAdapter);
+
+        filData();
+    }
+
+    private void filData() {
+    }
+
+}
