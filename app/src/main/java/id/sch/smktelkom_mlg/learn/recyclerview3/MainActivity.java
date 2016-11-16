781953d26f71478bc6944cdd60e5fd4c79d26e94
RinkoYukina <yukakanama@gmail.com>
2016-11-17 03:48:19 +0700

    RecyclerView Search

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,224 +1,117 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
-public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
-    public static final String HOTEL = "hotel";
-    public static final int REQUEST_CODE_ADD = 88;
-    public static final int REQUEST_CODE_EDIT = 99;
-    ArrayList<Hotel> mList = new ArrayList<>();
-    HotelAdapter mAdapter;
-    ArrayList<Hotel> mListAll = new ArrayList<>();
-    boolean isFiltered;
-    ArrayList<Integer> mListMapFilter = new ArrayList<>();
-    String mQuery;
+public class HotelAdapter extends RecyclerView.Adapter<id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.ViewHolder> {
+    ArrayList<Hotel> hotelList;
 
-    int itemPos;
+    id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.IHotelAdapter mIHotelAdapter;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goAdd();
-            }
-        });
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new HotelAdapter(this, mList);
-        recyclerView.setAdapter(mAdapter);
-
-        filData();
-    }
-
-    private void goAdd() {
-        startActivityForResult(new Intent(this, InputActivity.class), REQUEST_CODE_ADD);
-    }
-
-    private void filData() {
-        Resources resources = getResources();
-        String[] arJudul = resources.getStringArray(R.array.places);
-        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
-        String[] arDetail = resources.getStringArray(R.array.place_details);
-        String[] arLokasi = resources.getStringArray(R.array.place_locations);
-        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
-        String[] arFoto = new String[a.length()];
-        for (int i = 0; i < arFoto.length; i++) {
-            int id = a.getResourceId(i, 0);
-            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
-                    + resources.getResourcePackageName(id) + '/'
-                    + resources.getResourceTypeName(id) + '/'
-                    + resources.getResourceEntryName(id);
-        }
-        a.recycle();
-
-        for (int i = 0; i < arJudul.length; i++) {
-            mList.add(new Hotel(arJudul[i], arDeskripsi[i], arDetail[i],
-                    arLokasi[i], arFoto[i]));
-        }
-        mAdapter.notifyDataSetChanged();
+    public HotelAdapter(Context context, ArrayList<Hotel> hotelList) {
+        this.hotelList = hotelList;
+        mIHotelAdapter = (id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.IHotelAdapter) context;
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.ViewHolder vh = new id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.ViewHolder(v);
+        return vh;
+    }
 
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView)
-                MenuItemCompat.getActionView(searchItem);
+    @Override
+    public void onBindViewHolder(id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter.ViewHolder holder, int position) {
+        Hotel hotel = hotelList.get(position);
+        holder.tvJudul.setText(hotel.judul);
+        holder.tvDeskripsi.setText(hotel.deskripsi);
+        holder.ivFoto.setImageURI(Uri.parse(hotel.foto));
 
-        searchView.setOnQueryTextListener(
-                new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String query) {
-                        return false;
-                    }
+    }
 
-                    @Override
-                    public boolean onQueryTextChange(String newText) {
-                        mQuery = newText.toLowerCase();
-                        doFilter(mQuery);
-                        return true;
-                    }
+    @Override
+    public int getItemCount() {
+        if (hotelList != null)
+            return hotelList.size();
+        return 0;
+    }
+
+    public interface IHotelAdapter {
+        void doClick(int pos);
+
+        void doEdit(int pos);
+
+        void doDelete(int pos);
+
+        void doFav(int pos);
+
+        void doShare(int pos);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivFoto;
+        TextView tvJudul;
+        TextView tvDeskripsi;
+        Button bEdit;
+        Button bDelete;
+        ImageButton ibFav;
+        ImageButton ibShare;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
+            tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
+            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+            bEdit = (Button) itemView.findViewById(R.id.buttonEdit);
+            bDelete = (Button) itemView.findViewById(R.id.buttonDelete);
+            ibFav = (ImageButton) itemView.findViewById(R.id.buttonFavorite);
+            ibShare = (ImageButton) itemView.findViewById(R.id.buttonShare);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIHotelAdapter.doClick(getAdapterPosition());
                 }
-        );
-        return true;
-    }
+            });
 
-    private void doFilter(String query) {
-        if (!isFiltered) {
-            mListAll.clear();
-            mListAll.addAll(mList);
-            isFiltered = true;
-        }
-
-        mList.clear();
-        if (query == null || query.isEmpty()) {
-            mList.addAll(mListAll);
-            isFiltered = false;
-        } else {
-            mListMapFilter.clear();
-            for (int i = 0; i < mListAll.size(); i++) {
-                Hotel hotel = mListAll.get(i);
-                if (hotel.judul.toLowerCase().contains(query) ||
-                        hotel.deskripsi.toLowerCase().contains(query) ||
-                        hotel.lokasi.toLowerCase().contains(query)) {
-                    mList.add(hotel);
-                    mListMapFilter.add(i);
+            bEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIHotelAdapter.doEdit(getAdapterPosition());
                 }
-            }
-        }
-        mAdapter.notifyDataSetChanged();
-    }
+            });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            bDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIHotelAdapter.doDelete(getAdapterPosition());
+                }
+            });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            ibFav.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIHotelAdapter.doFav(getAdapterPosition());
+                }
+            });
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void doClick(int pos) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(HOTEL, mList.get(pos));
-        startActivity(intent);
-    }
-
-    @Override
-    public void doEdit(int pos) {
-        itemPos = pos;
-        Intent intent = new Intent(this, InputActivity.class);
-        intent.putExtra(HOTEL, mList.get(pos));
-        startActivityForResult(intent, REQUEST_CODE_EDIT);
-    }
-
-    @Override
-    public void doDelete(int pos) {
-        itemPos = pos;
-        final Hotel hotel = mList.get(pos);
-        mList.remove(itemPos);
-        if (isFiltered) mListAll.remove(mListMapFilter.get(itemPos).intValue());
-        mAdapter.notifyDataSetChanged();
-        Snackbar.make(findViewById(R.id.fab), hotel.judul + " Terhapus", Snackbar.LENGTH_LONG)
-                .setAction("UNDO", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mList.add(itemPos, hotel);
-                        if (isFiltered) mListAll.add(mListMapFilter.get(itemPos), hotel);
-                        mAdapter.notifyDataSetChanged();
-                    }
-                })
-                
-                .show();
-    }
-
-    @Override
-    public void doFav(int pos) {
-
-    }
-
-    @Override
-    public void doShare(int pos) {
-
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK) {
-            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
-            mList.add(hotel);
-            if (isFiltered) mListAll.add(hotel);
-            doFilter(mQuery);
-            //mAdapter.notifyDataSetChanged();
-        } else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK) {
-            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
-            mList.remove(itemPos);
-            if (isFiltered) mListAll.remove(mListMapFilter.get(itemPos).intValue());
-            mList.add(itemPos, hotel);
-            if (isFiltered) mListAll.add(mListMapFilter.get(itemPos), hotel);
-            mAdapter.notifyDataSetChanged();
+            ibShare.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIHotelAdapter.doShare(getAdapterPosition());
+                }
+            });
         }
     }
-}
 }
